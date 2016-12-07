b2c805143a2b798d382ca17fa9c4c3f468dc193b
AmberBadam <amber.badam@acorel.nl>
2016-12-07 14:17:59 +0100

    Sub process with Refresh tokens working

--- com.sap.api.hci.hybris.spike.one.exception.flow/src/main/resources/script/CheckTokenValidity.js
+++ com.sap.api.hci.hybris.spike.one.exception.flow/src/main/resources/script/CheckTokenValidity.js
@@ -8,13 +8,21 @@ function processData(message) {
 	
 //Calucuate the expiry date and time 	
 	var expiry = new Date(edate);
-	expiry.setSeconds(expiry.getSeconds()+etime);	
+//	expiry.setSeconds(expiry.getSeconds()+etime);
+    expiry.setTime(expiry.getTime()+parseInt(etime)*1000);  	
 	var currentdate = new Date();
-
 //set indicator to start new flow to fectch new token	
-	if(currentdate < expiry){
+	if(currentdate > expiry){
 		message.setProperty("refresh", "X");
 	}  
+//logging	
+/*   var messageLog = messageLogFactory.getMessageLog(message);	
+	    if(messageLog != null){
+        messageLog.setStringProperty("Logging properties", "Printing properties As Attachment")
+        messageLog.addAttachmentAsString("Prop:", expiry, "text/plain");
+        messageLog.addAttachmentAsString("Prop:", etime, "text/plain");
+    }; 
+*/    
 	return message;
 }
 
